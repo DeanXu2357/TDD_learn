843e2b48a277a362a6e68b62fb46161db021ec5c
py <jasugun0000@gmail.com>
2017-08-27 19:46:45 +0800

    新增fruit model

---
+++ app/Models/Fruits.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Fruits extends Model
+{
+    /**
+     * The table associated with the model.
+     * 這個model關連到的資料表名稱
+     *
+     * @var string
+     */
+    protected $table = 'fruits';
+}
