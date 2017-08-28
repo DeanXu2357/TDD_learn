ad0ed796b89f0bd1bfbbe9210450ae21a87632e2
py <jasugun0000@gmail.com>
2017-08-29 07:02:02 +0800

    新增 funciton for 取回使用者資料API

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -49,4 +49,26 @@ class AuthenticateController extends Controller
 
         JWTAuth::invalidate($request->input('token'));
     }
+
+    /**
+     * Returns the authenticated user
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function authenticatedUser()
+    {
+        try {
+            if (!$user = JWTAuth::parseToken()) {
+                return response()->json(['user_not_found'], 404);
+            }
+        } catch (\Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+            return response()->json(['token_expired'], $e->getStatusCode());
+        } catch (\Tymon\JWTAuth\Wxceptions\TokenInvalidException $e) {
+            return response()->json(['token_invalid'], $e->getStatusCode());
+        } catch (\Tymon\JWTAuth\Exceptions\JWTexception $e) {
+            return response()->json(['token_absent'], $e->getStatusCode());
+        }
+
+        return response()->json(compact('user'));
+    }
 }
