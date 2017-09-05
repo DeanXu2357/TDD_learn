c4bd3b594787ea931dd334f2a560c7241748f9e2
py <jasugun0000@gmail.com>
2017-09-05 23:36:32 +0800

    新增測試

--- tests/Feature/FirstClass.php
+++ tests/Feature/FirstClass.php
@@ -8,6 +8,10 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use DatabaseMigrations as DB;
 
+use JWTAuth;
+use App\Models\Users;
+use App\Models\Fruits;
+
 class FirstClass extends TestCase
 {
     /**
@@ -83,13 +87,41 @@ class FirstClass extends TestCase
         // TODO 缺點 每次執行測試都會創造一筆資料 之後找方法改進
         // $user = factory(\App\Models\Users::class)
         //     ->create(['password' => bcrypt('foo')]);
-        $this->post('/api/authenticate', ['email' => 'jacobi.dan@example.net', 'password' => 'foo'])->assertJsonStructure(['token']);
+        $user = Users::where('email', 'jacobi.dan@example.net')->first();
+
 
         $fruit = ['name' => 'peache', 'color' => 'peache', 'weight' => '175', 'delicious' => true];
 
-        // $this->post('/api/fruits', $fruit, $this->headers($user)->seeStatusCode(201));
+        // $this->post('/api/fruits', $fruit, $this->headers($user))->seeStatusCode(201);
+        $header = $this->headers($user);
+        // TODO 這邊在header 裡放使用者資訊似乎不太優 ， 待之後改進
+        $response = $this->call('POST', '/api/fruits', $fruit, [], [], $header);
 
-        $response = $this->call('POST', '/api/frusits', $fruit);
+        // $this->assertEquals($header, []);
         $this->assertEquals(201, $response->status());
+    }
+
+    public function headers($user = null)
+    {
+        $headers = ['HTTP_Accept' => 'application/json'];
+
+        if ($user) {
+            $token = JWTAuth::fromUser($user);
+            //JWTAuth::setToken($token);
+            $headers['HTTP_Authorization'] = 'Bearer '.$token;
+        }
+
+        return $headers;
+    }
+
+    /**
+     * @test
+     */
+    public function 沒有認證就創造水果()
+    {
+        $fruit = Fruits::create(['name' => 'peache', 'color' => 'peache', 'weight' => '175', 'delicious' => true])->toArray();
+
+        $response = $this->post('/api/fruits', $fruit);
+        $this->assertEquals('401', $response->status());
     }
 }
